b939e624e3af68e4f1ef5bdc098f6edf885d6d02
@garaidipak1998 <garaidipak1998@gmail.com>
2021-08-08 19:26:13 +0530

    Changed the style of FirstPage

--- src/FirstPage.js
+++ src/FirstPage.js
@@ -7,12 +7,13 @@ const imageLink3 = "https://picsum.photos/240/300";
 export default class FirstPage extends Component {
     render(){
         return(
-            <>
+            <div style={{width:"900px",margin:"auto"}}>
                 <h2>Random Images</h2>
+                <p>Images are changed in every refresh</p>
                 <img src={imageLink1}  alt="randomImage"/>
                 <img src={imageLink2}  alt="randomImage"/>
                 <img src={imageLink3}  alt="randomImage"/>
-            </>
+            </div>
         )
     }
 } 
