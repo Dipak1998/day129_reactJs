a08abd474bc76ed78d508bedb7b7f556a9787f51
@garaidipak1998 <garaidipak1998@gmail.com>
2021-08-21 18:29:27 +0530

    remove  App2.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react'
-
-export class App extends Component {
+// import './App.css';
+import Image from './Images/bgImage.jpg';
+import FirstPage from './FirstPage';
+import Props from './Props.js';
+const pTagStyling = {
+  color: "red",
+  backgroundColor: "aqua",
+  padding : "40px 80px",
+  margin: "80px"
+} 
+ 
+const user = {
+  name: "Dipak Garai",
+  id:3,
+  position: "Fullstack Developer"
+}
+const message = <div> <p>Hello Everyone, we are learning reactjs</p> </div>
+export default class App extends Component {
   render() {
     return (
-      <div>
-        
+      <div className="app">
+        <h1 style={{color:"green",border:"2px solid blue"}}>Welcome to codethunder</h1>
+        {message}
+        <p style= {pTagStyling}>We are learning React Js ...</p>
+        <Props user = {user}/>
       </div>
     )
   }
 }
-
-export default App
